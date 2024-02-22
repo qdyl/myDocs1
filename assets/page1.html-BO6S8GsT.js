7184c401824a112e287c6d78459c953d31e7a501
yuanquan <2187911790@qq.com>
2024-02-22 22:58:26 +0800

    '提交'

---
+++ assets/page1.html-BO6S8GsT.js
@@ -0,0 +1,51 @@
+import{_ as p}from"./plugin-vue_export-helper-x3n3nnut.js";import{r as l,o as c,c as o,b as u,w as a,d as n,f as s,e as r}from"./app-E2yLQ0Pc.js";const d={},v=n("h2",{id:"常用-class-类名",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#常用-class-类名","aria-hidden":"true"},"#"),s(" 常用 class 类名")],-1),k=n("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token function"},"pnpm"),s(),n("span",{class:"token function"},"add"),s(),n("span",{class:"token parameter variable"},"-D"),s(` vuepress-theme-hope
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"})])],-1),m=n("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token function"},"yarn"),s(),n("span",{class:"token function"},"add"),s(),n("span",{class:"token parameter variable"},"-D"),s(` vuepress-theme-hope
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"})])],-1),b=n("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token function"},"npm"),s(" i "),n("span",{class:"token parameter variable"},"-D"),s(` vuepress-theme-hope
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"})])],-1),h=r(`<h2 id="css-基础样式-重置" tabindex="-1"><a class="header-anchor" href="#css-基础样式-重置" aria-hidden="true">#</a> css 基础样式(重置)</h2><details><summary>点击折叠/展开</summary><div class="language-css line-numbers-mode" data-ext="css"><pre class="language-css"><code><span class="token comment">/* 样式重置 */</span>
+<span class="token selector">*</span><span class="token punctuation">{</span>
+  <span class="token property">margin</span><span class="token punctuation">:</span> 0<span class="token punctuation">;</span>
+  <span class="token property">padding</span><span class="token punctuation">:</span> 0<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+<span class="token selector">a</span><span class="token punctuation">{</span>
+  <span class="token property">text-decoration</span><span class="token punctuation">:</span> none<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+<span class="token selector">ul,ol,li</span><span class="token punctuation">{</span>
+  <span class="token property">list-style-type</span><span class="token punctuation">:</span> none<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+
+<span class="token comment">/* 工具类 */</span>
+
+<span class="token selector">.fleft</span><span class="token punctuation">{</span>
+  <span class="token property">float</span><span class="token punctuation">:</span>left
+<span class="token punctuation">}</span>
+<span class="token selector">.fright</span><span class="token punctuation">{</span>
+  <span class="token property">float</span><span class="token punctuation">:</span> right<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+<span class="token selector">.clearfix:before,.clearfix:after</span><span class="token punctuation">{</span>
+  <span class="token property">content</span><span class="token punctuation">:</span> <span class="token string">&#39;&#39;</span><span class="token punctuation">;</span>
+  <span class="token property">display</span><span class="token punctuation">:</span> block<span class="token punctuation">;</span>
+  <span class="token property">height</span><span class="token punctuation">:</span> 0<span class="token punctuation">;</span>
+  <span class="token property">clear</span><span class="token punctuation">:</span> both<span class="token punctuation">;</span>
+  <span class="token property">visibility</span><span class="token punctuation">:</span> hidden<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+<span class="token selector">// 单行省略号
+.ellipsis</span><span class="token punctuation">{</span>
+  <span class="token property">overflow</span><span class="token punctuation">:</span> hidden<span class="token punctuation">;</span>
+  <span class="token property">white-space</span><span class="token punctuation">:</span> nowrap<span class="token punctuation">;</span>
+  <span class="token property">text-overflow</span><span class="token punctuation">:</span> ellipsis<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+<span class="token selector">.ellipsis2</span><span class="token punctuation">{</span>
+  <span class="token property">display</span><span class="token punctuation">:</span> -webkit-box<span class="token punctuation">;</span>
+  <span class="token property">-webkit-box-orient</span><span class="token punctuation">:</span> vertical<span class="token punctuation">;</span>
+  <span class="token property">-webkit-line-clamp</span><span class="token punctuation">:</span> 2<span class="token punctuation">;</span>
+  <span class="token property">overflow</span><span class="token punctuation">:</span> hidden<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+<span class="token selector">.ellipsis3</span><span class="token punctuation">{</span>
+  <span class="token property">display</span><span class="token punctuation">:</span> -webkit-box<span class="token punctuation">;</span>
+  <span class="token property">-webkit-box-orient</span><span class="token punctuation">:</span> vertical<span class="token punctuation">;</span>
+  <span class="token property">-webkit-line-clamp</span><span class="token punctuation">:</span> 3<span class="token punctuation">;</span>
+  <span class="token property">overflow</span><span class="token punctuation">:</span> hidden<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></details><h2 id="形状" tabindex="-1"><a class="header-anchor" href="#形状" aria-hidden="true">#</a> 形状</h2><h3 id="下拉三角" tabindex="-1"><a class="header-anchor" href="#下拉三角" aria-hidden="true">#</a> 下拉三角</h3><h2 id="渐变" tabindex="-1"><a class="header-anchor" href="#渐变" aria-hidden="true">#</a> 渐变</h2><h3 id="蓝色渐变背景" tabindex="-1"><a class="header-anchor" href="#蓝色渐变背景" aria-hidden="true">#</a> 蓝色渐变背景</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>background:linear-gradient(180deg, #0193FE 0%, #F8F8F8 100%);
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><figure><img src="https://gitee.com/qdshaonianlang/imgs/raw/master/img/image-20240206145751365.png" alt="image-20240206145751365" width="80" height="45" tabindex="0" loading="lazy"><figcaption>image-20240206145751365</figcaption></figure>`,8);function g(f,y){const i=l("CodeTabs");return c(),o("div",null,[v,u(i,{id:"3",data:[{id:"pnpm"},{id:"yarn"},{id:"npm"}],active:2,"tab-id":"shell"},{title0:a(({value:e,isActive:t})=>[s("pnpm")]),title1:a(({value:e,isActive:t})=>[s("yarn")]),title2:a(({value:e,isActive:t})=>[s("npm")]),tab0:a(({value:e,isActive:t})=>[k]),tab1:a(({value:e,isActive:t})=>[m]),tab2:a(({value:e,isActive:t})=>[b]),_:1}),h])}const w=p(d,[["render",g],["__file","page1.html.vue"]]);export{w as default};
---
+++ assets/page1.html-BYtZtvXI.js
@@ -0,0 +1,95 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{r as i,o,c as u,d as n,f as s,b as l,e as t}from"./app-E2yLQ0Pc.js";const c={},r=t(`<h2 id="介绍" tabindex="-1"><a class="header-anchor" href="#介绍" aria-hidden="true">#</a> 介绍</h2><h2 id="vscode配置" tabindex="-1"><a class="header-anchor" href="#vscode配置" aria-hidden="true">#</a> vscode配置</h2><details><summary>点击折叠/展开</summary><div class="language-JSON line-numbers-mode" data-ext="JSON"><pre class="language-JSON"><code>{
+  // 自动保存
+  &quot;editor.formatOnSave&quot;: true,
+  &quot;editor.codeActionsOnSave&quot;: {
+      &quot;source.fixAll.eslint&quot;: &quot;explicit&quot;
+  },
+  // 字体、 菜单栏字体
+  // &quot;editor.fontSize&quot;: 14,
+  // &quot;window.zoomLevel&quot;: -0.8,
+  // &quot;window.zoomLevel&quot;: -0.4,
+  //
+  &quot;files.eol&quot;: &quot;\\n&quot;,
+  &quot;eslint.validate&quot;: [
+      &quot;javascript&quot;,
+      &quot;vue&quot;,
+      &quot;html&quot;,
+      &quot;json&quot;
+  ],
+  // &quot;liveServer.settings.AdvanceCustomBrowserCmdLine&quot;: &quot;C:/Users/dell/AppData/Local/Google/Chrome/Application/chrome.exe&quot;,
+  // sass
+  &quot;easysass.formats&quot;: [
+      {
+          &quot;format&quot;: &quot;expanded&quot;,
+          &quot;extension&quot;: &quot;.css&quot;
+      },
+      {
+          &quot;format&quot;: &quot;compressed&quot;,
+          &quot;extension&quot;: &quot;.min.css&quot;
+      }
+  ],
+  &quot;workbench.colorCustomizations&quot;: {
+      &quot;activityBar.activeBackground&quot;: &quot;#fa1b49&quot;,
+      &quot;activityBar.activeBorder&quot;: &quot;#dd2233&quot;,
+      &quot;activityBar.background&quot;: &quot;#fa1b49&quot;,
+      &quot;activityBar.foreground&quot;: &quot;#e7e7e7&quot;,
+      &quot;activityBar.inactiveForeground&quot;: &quot;#e7e7e799&quot;,
+      &quot;activityBarBadge.background&quot;: &quot;#155e02&quot;,
+      &quot;activityBarBadge.foreground&quot;: &quot;#e7e7e7&quot;,
+      &quot;sash.hoverBorder&quot;: &quot;#fa1b49&quot;,
+      &quot;statusBar.background&quot;: &quot;#dd0531&quot;,
+      &quot;statusBar.foreground&quot;: &quot;#e7e7e7&quot;,
+      &quot;statusBarItem.hoverBackground&quot;: &quot;#fa1b49&quot;,
+      &quot;statusBarItem.remoteBackground&quot;: &quot;#dd0531&quot;,
+      &quot;statusBarItem.remoteForeground&quot;: &quot;#e7e7e7&quot;,
+      &quot;titleBar.activeBackground&quot;: &quot;#dd0531&quot;,
+      &quot;titleBar.activeForeground&quot;: &quot;#e7e7e7&quot;,
+      &quot;titleBar.inactiveBackground&quot;: &quot;#dd053199&quot;,
+      &quot;titleBar.inactiveForeground&quot;: &quot;#e7e7e799&quot;,
+      &quot;commandCenter.border&quot;: &quot;#e7e7e799&quot;
+  },
+  &quot;peacock.color&quot;: &quot;#dd0531&quot;
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></details><h2 id="常用扩展" tabindex="-1"><a class="header-anchor" href="#常用扩展" aria-hidden="true">#</a> 常用扩展</h2><ul><li></li></ul><h2 id="git使用" tabindex="-1"><a class="header-anchor" href="#git使用" aria-hidden="true">#</a> Git使用</h2><h2 id="调试" tabindex="-1"><a class="header-anchor" href="#调试" aria-hidden="true">#</a> 调试</h2>`,7),d={href:"/TopicCategory/PacketCapture_Debug/",target:"_blank",rel:"noopener noreferrer"},p=t(`<h2 id="使用技巧" tabindex="-1"><a class="header-anchor" href="#使用技巧" aria-hidden="true">#</a> 使用技巧</h2><h3 id="复制相对路径" tabindex="-1"><a class="header-anchor" href="#复制相对路径" aria-hidden="true">#</a> 复制相对路径</h3><blockquote><p>文件-》首选项-》设置-》搜索 copy relative path -》 选择 /</p></blockquote><details><summary>点击折叠/展开</summary><p><img src="https://gitee.com/qdshaonianlang/imgs/raw/master/img/image-20230830104215084.png" alt="image-20230830104215084" width="200" height="110" loading="lazy"><img src="https://gitee.com/qdshaonianlang/imgs/raw/master/img/image-20230830104310810.png" alt="image-20230830104310810" width="200" height="110" loading="lazy"></p></details><h2 id="配置模板" tabindex="-1"><a class="header-anchor" href="#配置模板" aria-hidden="true">#</a> 配置模板</h2><h3 id="vue-模板" tabindex="-1"><a class="header-anchor" href="#vue-模板" aria-hidden="true">#</a> vue 模板</h3><details><summary>折叠/展开</summary><div class="language-json line-numbers-mode" data-ext="json"><pre class="language-json"><code><span class="token punctuation">{</span>
+<span class="token property">&quot;Vue2.x快速生成模板&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+  <span class="token property">&quot;prefix&quot;</span><span class="token operator">:</span> <span class="token string">&quot;vue2&quot;</span><span class="token punctuation">,</span>
+  <span class="token property">&quot;body&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+    <span class="token string">&quot;&lt;!-- $1 --&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;template&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t&lt;div class=\\&quot;\\&quot;&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t\\t$3&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t&lt;/div&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;/template&gt;\\n&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;script&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\texport default {&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t\\tdata(){return {}}&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t}&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t$2&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;/script&gt;\\n&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;style lang=\\&quot;scss\\&quot; scoped&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t$4&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;/style&gt;&quot;</span>
+  <span class="token punctuation">]</span><span class="token punctuation">,</span>
+  <span class="token property">&quot;description&quot;</span><span class="token operator">:</span> <span class="token string">&quot;Vue2.x&quot;</span>
+<span class="token punctuation">}</span><span class="token punctuation">,</span>
+<span class="token property">&quot;Vue3.2快速生成模板&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+  <span class="token property">&quot;prefix&quot;</span><span class="token operator">:</span> <span class="token string">&quot;vue3&quot;</span><span class="token punctuation">,</span>
+  <span class="token property">&quot;body&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+    <span class="token string">&quot;&lt;!-- $1 --&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;template&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t&lt;div class=\\&quot;\\&quot;&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t\\t$3&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t&lt;/div&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;/template&gt;\\n&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;script setup&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;import {} from &#39;vue&#39;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t$2&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;/script&gt;\\n&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;style lang=\\&quot;scss\\&quot; scoped&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t$4&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;/style&gt;&quot;</span>
+  <span class="token punctuation">]</span><span class="token punctuation">,</span>
+  <span class="token property">&quot;description&quot;</span><span class="token operator">:</span> <span class="token string">&quot;Vue3.x&quot;</span>
+<span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></details>`,7);function v(q,m){const a=i("ExternalLinkIcon");return o(),u("div",null,[r,n("blockquote",null,[n("p",null,[s("详细见：抓包调试。 "),n("a",d,[s("链接"),l(a)])])]),p])}const g=e(c,[["render",v],["__file","page1.html.vue"]]);export{g as default};
