774bd7ed53db33799ea432718dded145a2f8b5c1
yuanquan <2187911790@qq.com>
2024-03-12 23:35:45 +0800

    提交信息

---
+++ assets/page1.html-gVo-pbuh.js
@@ -0,0 +1,99 @@
+import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as e,c as i,d as s}from"./app-_yw3Vdmw.js";const l={},d=s(`<h2 id="_1" tabindex="-1"><a class="header-anchor" href="#_1" aria-hidden="true">#</a> 1</h2><h3 id="默认原始文件" tabindex="-1"><a class="header-anchor" href="#默认原始文件" aria-hidden="true">#</a> 默认原始文件</h3><details><summary>更多</summary><blockquote><p>目录 : etc-&gt;nginx-&gt;nginx.conf</p></blockquote><div class="language-json line-numbers-mode" data-ext="json"><pre class="language-json"><code># For more information on configuration<span class="token punctuation">,</span> see<span class="token operator">:</span>
+#   * Official English Documentation<span class="token operator">:</span> http<span class="token operator">:</span><span class="token comment">//nginx.org/en/docs/</span>
+#   * Official Russian Documentation<span class="token operator">:</span> http<span class="token operator">:</span><span class="token comment">//nginx.org/ru/docs/</span>
+
+user nginx;
+worker_processes auto;
+error_log /var/log/nginx/error.log;
+pid /run/nginx.pid;
+
+# Load dynamic modules. See /usr/share/doc/nginx/README.dynamic.
+include /usr/share/nginx/modules<span class="token comment">/*.conf;
+
+events {
+    worker_connections 1024;
+}
+
+http {
+    log_format  main  &#39;$remote_addr - $remote_user [$time_local] &quot;$request&quot; &#39;
+                      &#39;$status $body_bytes_sent &quot;$http_referer&quot; &#39;
+                      &#39;&quot;$http_user_agent&quot; &quot;$http_x_forwarded_for&quot;&#39;;
+
+    access_log  /var/log/nginx/access.log  main;
+
+    sendfile            on;
+    tcp_nopush          on;
+    tcp_nodelay         on;
+    keepalive_timeout   65;
+    types_hash_max_size 2048;
+
+    include             /etc/nginx/mime.types;
+    default_type        application/octet-stream;
+
+    # Load modular configuration files from the /etc/nginx/conf.d directory.
+    # See http://nginx.org/en/docs/ngx_core_module.html#include
+    # for more information.
+    include /etc/nginx/conf.d/*.conf;
+
+    server {
+        listen       80 default_server;
+        listen       [::]:80 default_server;
+        server_name  _;
+        root         /usr/share/nginx/html;
+
+        # Load configuration files for the default server block.
+        include /etc/nginx/default.d/*.conf;
+
+        location / {
+        }
+
+        error_page 404 /404.html;
+            location = /40x.html {
+        }
+
+        error_page 500 502 503 504 /50x.html;
+            location = /50x.html {
+        }
+    }
+# 新增
+    server {
+        listen       80 default_server;
+        listen       [::]:80 default_server;
+        server_name  _;
+        root         /yweb/vue/pm/vue2pm/dist;
+     }
+
+# Settings for a TLS enabled server.
+#
+#    server {
+#        listen       443 ssl http2 default_server;
+#        listen       [::]:443 ssl http2 default_server;
+#        server_name  _;
+#        root         /usr/share/nginx/html;
+#
+#        ssl_certificate &quot;/etc/pki/nginx/server.crt&quot;;
+#        ssl_certificate_key &quot;/etc/pki/nginx/private/server.key&quot;;
+#        ssl_session_cache shared:SSL:1m;
+#        ssl_session_timeout  10m;
+#        ssl_ciphers PROFILE=SYSTEM;
+#        ssl_prefer_server_ciphers on;
+#
+#        # Load configuration files for the default server block.
+#        include /etc/nginx/default.d/*.conf;
+#
+#        location / {
+#        }
+#
+#        error_page 404 /404.html;
+#            location = /40x.html {
+#        }
+#
+#        error_page 500 502 503 504 /50x.html;
+#            location = /50x.html {
+#        }
+#    }
+
+}
+
+
+</span></code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></details><figure><img src="https://gitee.com/qdshaonianlang/imgs/raw/master/img/image-20240228120521496.png" alt="image-20240228120521496" tabindex="0" loading="lazy"><figcaption>image-20240228120521496</figcaption></figure>`,4),r=[d];function a(v,c){return e(),i("div",null,r)}const u=n(l,[["render",a],["__file","page1.html.vue"]]);export{u as default};
---
+++ assets/page2.html-f8IcYI--.js
@@ -0,0 +1,43 @@
+import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as s,c as a,d as t}from"./app-_yw3Vdmw.js";const e={},o=t(`<h2 id="技巧-效率" tabindex="-1"><a class="header-anchor" href="#技巧-效率" aria-hidden="true">#</a> 技巧~效率</h2><h3 id="vue-组件跳转" tabindex="-1"><a class="header-anchor" href="#vue-组件跳转" aria-hidden="true">#</a> vue 组件跳转</h3><blockquote><p>不知道是不是 vscode 版本升级的原因，现在可以直接在组件名上，按住 ctrl + 鼠标左键 ， 可以进行文件跳转（vue-cli 3.x 前端工程中）。</p></blockquote><p><img src="https://gitee.com/qdshaonianlang/imgs/raw/master/img/image-20240312113136472.png" alt="image-20240312113136472" width="200" height="110" loading="lazy"><img src="https://gitee.com/qdshaonianlang/imgs/raw/master/img/1111556933888856895662355521.gif" alt="1111556933888856895662355521" width="200" height="110" loading="lazy"></p><h3 id="复制相对路径" tabindex="-1"><a class="header-anchor" href="#复制相对路径" aria-hidden="true">#</a> 复制相对路径</h3><blockquote><p>文件-》首选项-》设置-》搜索 copy relative path -》 选择 /</p></blockquote><p><img src="https://gitee.com/qdshaonianlang/imgs/raw/master/img/image-20230830104215084.png" alt="image-20230830104215084" width="200" height="110" loading="lazy"><img src="https://gitee.com/qdshaonianlang/imgs/raw/master/img/image-20230830104310810.png" alt="image-20230830104310810" width="200" height="110" loading="lazy"></p><h2 id="配置模板" tabindex="-1"><a class="header-anchor" href="#配置模板" aria-hidden="true">#</a> 配置模板</h2><h3 id="vue-模板" tabindex="-1"><a class="header-anchor" href="#vue-模板" aria-hidden="true">#</a> vue 模板</h3><details><summary>折叠/展开</summary><div class="language-json line-numbers-mode" data-ext="json"><pre class="language-json"><code><span class="token punctuation">{</span>
+<span class="token property">&quot;Vue2.x快速生成模板&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+  <span class="token property">&quot;prefix&quot;</span><span class="token operator">:</span> <span class="token string">&quot;vue2&quot;</span><span class="token punctuation">,</span>
+  <span class="token property">&quot;body&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+    <span class="token string">&quot;&lt;!-- $1 --&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;template&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t&lt;div class=\\&quot;\\&quot;&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t\\t$3&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t&lt;/div&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;/template&gt;\\n&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;script&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\texport default {&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t\\tdata(){return {}}&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t}&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t$2&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;/script&gt;\\n&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;style lang=\\&quot;scss\\&quot; scoped&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t$4&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;/style&gt;&quot;</span>
+  <span class="token punctuation">]</span><span class="token punctuation">,</span>
+  <span class="token property">&quot;description&quot;</span><span class="token operator">:</span> <span class="token string">&quot;Vue2.x&quot;</span>
+<span class="token punctuation">}</span><span class="token punctuation">,</span>
+<span class="token property">&quot;Vue3.2快速生成模板&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+  <span class="token property">&quot;prefix&quot;</span><span class="token operator">:</span> <span class="token string">&quot;vue3&quot;</span><span class="token punctuation">,</span>
+  <span class="token property">&quot;body&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+    <span class="token string">&quot;&lt;!-- $1 --&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;template&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t&lt;div class=\\&quot;\\&quot;&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t\\t$3&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t&lt;/div&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;/template&gt;\\n&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;script setup&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;import {} from &#39;vue&#39;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t$2&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;/script&gt;\\n&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;style lang=\\&quot;scss\\&quot; scoped&gt;&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;\\t$4&quot;</span><span class="token punctuation">,</span>
+    <span class="token string">&quot;&lt;/style&gt;&quot;</span>
+  <span class="token punctuation">]</span><span class="token punctuation">,</span>
+  <span class="token property">&quot;description&quot;</span><span class="token operator">:</span> <span class="token string">&quot;Vue3.x&quot;</span>
+<span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></details>`,10),i=[o];function p(l,u){return s(),a("div",null,i)}const d=n(e,[["render",p],["__file","page2.html.vue"]]);export{d as default};
